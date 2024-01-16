69e39d8d397024ac7eeeaa64f44d34d52d1b3eb4
ClayYu <32875832+yuhuajing@users.noreply.github.com>
2024-01-16 11:42:49 +0800

    Create RecordMintState.sol

---
+++ RecordMintState.sol
@@ -0,0 +1,480 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.20;
+
+import "erc721a/contracts/ERC721A.sol";
+import "https://github.com/LayerZero-Labs/solidity-examples/blob/main/contracts/token/onft721/ONFT721Core.sol";
+import "@openzeppelin/contracts/utils/cryptography/SignatureChecker.sol";
+import "./ISRCNFT.sol";
+import {Math} from "@openzeppelin/contracts/utils/math/Math.sol";
+
+contract ONFT721A is ONFT721Core, ERC721A, ERC721A__IERC721Receiver, ISRCNFT {
+    using Math for uint256;
+    // Whether this contract is mintable.
+    bool public mintable;
+
+    // The address of the cosigner server.
+    address public cosigner;
+
+    address public beneficiary;
+
+    // Current base URI.
+    // string public currentBaseURI;
+
+    // // The suffix for the token URL, e.g. ".json".
+    // string public tokenURISuffix;
+
+    uint256 private constant _One_HOUR = 1 hours;
+    uint256 private constant _One_DAY = 1 days;
+    uint256 private constant one_minute = 1 minutes;
+    uint256 public expireTime;
+
+    // Mint stage infomation. See MintStageInfo for details.
+    MintStageInfo[] public mintStages;
+
+    // Minted count per stage.
+    mapping(uint256 => uint256) public stageMintedCounts;
+
+    // Minted tokenID stakeDays.
+    mapping(uint256 => uint256) public tokenIdStakeDays;
+    mapping(string => bool) private ridvalue;
+
+    mapping(address => mapping(uint256 => uint256)) private stageMaxSupply;
+    uint256 public maxSupply;
+    error notSatifiedSig();
+
+    constructor(
+        string memory _name,
+        string memory _symbol,
+        // string memory _tokenURISuffix,
+        // string memory _currentBaseURI,
+        address _cosigner,
+        address _beneficiary,
+        uint256 _minGasToTransferAndStore,
+        address _lzEndpoint,
+        uint32 _expire,
+        uint256 _maxSupply
+    )
+        payable
+        ERC721A(_name, _symbol)
+        ONFT721Core(_minGasToTransferAndStore, _lzEndpoint)
+        Ownable(msg.sender)
+    {
+        mintable = true;
+        // tokenURISuffix = _tokenURISuffix;
+        // currentBaseURI = _currentBaseURI;
+        cosigner = _cosigner; // ethers.constants.AddressZero for no cosigning
+        beneficiary = _beneficiary;
+        expireTime = _expire * one_minute;
+        maxSupply = _maxSupply;
+    }
+
+    /**
+     * @dev Sets mintable.
+     */
+    function setMintable(bool _mintable) external onlyOwner {
+        mintable = _mintable;
+    }
+
+    function updateExpire(uint32 _expire) external onlyOwner {
+        expireTime = _expire * one_minute;
+    }
+
+    /**
+     * @dev Sets cosigner.
+     */
+    function updateCosigner(address _cosigner) external onlyOwner {
+        cosigner = _cosigner;
+    }
+
+    /**
+     * @dev Sets stages in the format of an array of `MintStageInfo`.
+     */
+    function setStages(InputMintStageInfo[] calldata newStages)
+        external
+        onlyOwner
+    {
+        uint256 newSize = newStages.length;
+        require((newSize != 0), "NO_STAGE");
+        uint256 originalSize = mintStages.length;
+        for (uint256 i = 0; i < originalSize; i++) {
+            mintStages.pop();
+        }
+
+        for (uint256 i = 0; i < newStages.length; i++) {
+            if (i >= 1) {
+                _assertValidStartAndEndTimestamp(
+                    newStages[i].startTimeUnixSeconds,
+                    mintStages[i - 1].endTimeUnixSeconds
+                );
+            }
+            mintStages.push(
+                MintStageInfo({
+                    whiteSalePrice: newStages[i].whiteSalePrice,
+                    publicSalePrice: newStages[i].publicSalePrice,
+                    maxStageSupply: newStages[i].maxStageSupply,
+                    whiteSaleHour: newStages[i].whiteSaleHour,
+                    publicSaleHour: newStages[i].publicSaleHour,
+                    startTimeUnixSeconds: newStages[i].startTimeUnixSeconds,
+                    endWhiteTimeUnixSeconds: newStages[i].startTimeUnixSeconds +
+                        newStages[i].whiteSaleHour *
+                        _One_HOUR,
+                    endTimeUnixSeconds: newStages[i].startTimeUnixSeconds +
+                        (newStages[i].publicSaleHour +
+                            newStages[i].whiteSaleHour) *
+                        _One_HOUR
+                })
+            );
+        }
+    }
+
+    /**
+     * @dev Updates info for one stage specified by index (starting from 0).
+     */
+    function updateStage(
+        uint256 index,
+        uint80 whiteSalePrice,
+        uint80 publicSalePrice,
+        uint24 maxStageSupply,
+        uint64 startTimeUnixSeconds,
+        uint24 whiteSaleHour,
+        uint24 publicSaleHour
+    ) external onlyOwner {
+        if (index >= mintStages.length) revert InvalidStage();
+        if (index >= 1) {
+            _assertValidStartAndEndTimestamp(
+                startTimeUnixSeconds,
+                mintStages[index - 1].endTimeUnixSeconds
+            );
+        }
+        mintStages[index].whiteSalePrice = whiteSalePrice;
+        mintStages[index].publicSalePrice = publicSalePrice;
+        mintStages[index].maxStageSupply = maxStageSupply;
+        mintStages[index].startTimeUnixSeconds = startTimeUnixSeconds;
+        mintStages[index].whiteSaleHour = whiteSaleHour;
+        mintStages[index].publicSaleHour = publicSaleHour;
+        mintStages[index].endWhiteTimeUnixSeconds =
+            startTimeUnixSeconds +
+            whiteSaleHour *
+            _One_HOUR;
+        mintStages[index].endTimeUnixSeconds =
+            startTimeUnixSeconds +
+            (publicSaleHour + whiteSaleHour) *
+            _One_HOUR;
+    }
+
+    function decode(bytes memory data)
+        internal
+        pure
+        returns (
+            uint32 qty,
+            string memory requestId,
+            uint64 timestamp,
+            bytes memory sig
+        )
+    {
+        (, , , qty, , requestId, timestamp, sig) = abi.decode(
+            data,
+            (address, address, address, uint32, uint32, string, uint64, bytes)
+        );
+    }
+
+    function assertValidCosign(bytes memory data)
+        internal
+        returns (uint32, uint64)
+    {
+        (
+            uint32 qty,
+            string memory requestId,
+            uint64 timestamp,
+            bytes memory sig
+        ) = decode(data);
+        require((expireTime + timestamp >= block.timestamp), "HAS_Expired");
+        require((!ridvalue[requestId]), "HAS_USED");
+        ridvalue[requestId] = true;
+        if (
+            !SignatureChecker.isValidSignatureNow(
+                cosigner,
+                getCosignDigest(
+                    msg.sender,
+                    qty,
+                    _chainID(),
+                    requestId,
+                    timestamp
+                ),
+                sig
+            )
+        ) {
+            revert notSatifiedSig();
+        }
+        return (qty, timestamp);
+    }
+
+    function mint(bytes calldata data) external payable virtual nonReentrant {
+        (uint32 qty, uint64 timestamp) = assertValidCosign(data);
+        _mintInternal(qty, msg.sender, timestamp);
+    }
+
+    function mint_to(address to, bytes calldata data)
+        external
+        payable
+        virtual
+        nonReentrant
+    {
+        (uint32 qty, uint64 timestamp) = assertValidCosign(data);
+        _mintInternal(qty, to, timestamp);
+    }
+
+    /**
+     * @dev Implementation of minting.
+     */
+    function _mintInternal(
+        uint32 qty,
+        address to,
+        uint64 stageTimestamp
+    ) internal {
+        (bool success, uint256 mintnum) = Math.tryMod(uint256(qty), 100);
+        require(success);
+        (bool _success, uint256 MaxMum) = Math.tryDiv(uint256(qty), 10000);
+        require(_success);
+
+        require(
+            (totalSupply() + mintnum <= maxSupply),
+            "exceed the max supply"
+        );
+        if (!mintable) revert NotMintable();
+
+        // uint64 stageTimestamp = uint64(block.timestamp);
+        _assertValidTimestamp(stageTimestamp);
+
+        uint256 activeStage = getActiveStageFromTimestamp(stageTimestamp);
+
+        require(
+            (stageMaxSupply[msg.sender][activeStage] + mintnum < MaxMum),
+            "Over_Mint"
+        );
+        stageMaxSupply[msg.sender][activeStage] += mintnum;
+        if (
+            stageMintedCounts[activeStage] + mintnum >
+            mintStages[activeStage].maxStageSupply
+        ) revert NoSupplyLeft();
+
+        uint256 price = mintStages[activeStage].publicSalePrice;
+        if (stageTimestamp <= mintStages[activeStage].endWhiteTimeUnixSeconds) {
+            price = mintStages[activeStage].whiteSalePrice;
+        }
+        if (msg.sender != owner()) {
+            // Check value if minting with ETH
+            if (msg.value < price * mintnum) revert NotEnoughValue();
+        }
+
+        stageMintedCounts[activeStage] += mintnum;
+        _safeMint(to, mintnum);
+    }
+
+    function burn(uint256 tokenId) public virtual {
+        require((checkStakeEnd(tokenId)), "TokenId_IN_STAKE");
+        _burn(tokenId, true);
+    }
+
+    function stake(uint256 tokenId, uint24 _days) public virtual {
+        require((ownerOf(tokenId) == msg.sender), "NOT_TOKENID_OWNER");
+        require((tokenIdStakeDays[tokenId] == 0), "IN_STAKE");
+        tokenIdStakeDays[tokenId] = block.timestamp + _days * _One_DAY;
+    }
+
+    function transferFrom(
+        address from,
+        address to,
+        uint256 _tokenId
+    ) public payable virtual override {
+        require((checkStakeEnd(_tokenId)), "TokenId_IN_STAKE");
+        super.transferFrom(from, to, _tokenId);
+    }
+
+    function checkStakeEnd(uint256 tokenId) internal view returns (bool) {
+        uint256 endTime = tokenIdStakeDays[tokenId];
+        return block.timestamp >= endTime;
+    }
+
+    function withdraw() external onlyOwner {
+        require(msg.sender == beneficiary);
+        uint256 value = address(this).balance;
+        (bool success, ) = msg.sender.call{value: value}("");
+        if (!success) revert WithdrawFailed();
+        emit Withdraw(value);
+    }
+
+    /**
+     * @dev Sets token base URI.
+     */
+    // function setBaseURI(string calldata baseURI) external onlyOwner {
+    //     currentBaseURI = baseURI;
+    // }
+
+    // /**
+    //  * @dev Sets token URI suffix. e.g. ".json".
+    //  */
+    // function setTokenURISuffix(string calldata suffix) external onlyOwner {
+    //     tokenURISuffix = suffix;
+    // }
+
+    /**
+     * @dev Returns token URI for a given token id.
+     */
+    // function tokenURI(uint256 tokenId)
+    //     public
+    //     view
+    //     override(ERC721A)
+    //     returns (string memory)
+    // {
+    //     if (!_exists(tokenId)) revert URIQueryForNonexistentToken();
+
+    //     string memory baseURI = currentBaseURI;
+    //     return
+    //         bytes(baseURI).length != 0
+    //             ? string(
+    //                 abi.encodePacked(
+    //                     baseURI,
+    //                     _toString(tokenId),
+    //                     tokenURISuffix
+    //                 )
+    //             )
+    //             : "";
+    // }
+
+    /**
+     * @dev Returns data hash for the given minter, qty and timestamp.
+     */
+    function getCosignDigest(
+        address sender,
+        uint32 qty,
+        uint32 chainId,
+        string memory requestId,
+        uint64 timestamp
+    ) internal view returns (bytes32) {
+        bytes32 _msgHash = keccak256(
+            abi.encodePacked(
+                address(this),
+                sender,
+                cosigner,
+                qty,
+                chainId,
+                requestId,
+                timestamp
+            )
+        );
+        return
+            keccak256(
+                abi.encodePacked("\x19Ethereum Signed Message:\n32", _msgHash)
+            );
+    }
+
+    /**
+     * @dev Returns the current active stage based on timestamp.
+     */
+    function getActiveStageFromTimestamp(uint64 timestamp)
+        public
+        view
+        returns (uint256)
+    {
+        for (uint256 i = 0; i < mintStages.length; i++) {
+            if (
+                timestamp >= mintStages[i].startTimeUnixSeconds &&
+                timestamp < mintStages[i].endTimeUnixSeconds
+            ) {
+                return i;
+            }
+        }
+        revert InvalidStage();
+    }
+
+    /**
+     * @dev Validates the timestamp is not expired.
+     */
+    function _assertValidTimestamp(uint64 timestamp) internal view {
+        uint256 originalSize = mintStages.length;
+        require((originalSize != 0), "NO_STAGE_SET");
+        _assertValidStartAndEndTimestamp(
+            mintStages[originalSize - 1].endTimeUnixSeconds,
+            timestamp
+        );
+        if (timestamp < mintStages[0].startTimeUnixSeconds)
+            revert TimestampExpired();
+    }
+
+    /**
+     * @dev Validates the start timestamp is before end timestamp. Used when updating stages.
+     */
+    function _assertValidStartAndEndTimestamp(uint256 start, uint256 end)
+        internal
+        pure
+    {
+        if (start < end) revert InvalidStartAndEndTimestamp();
+    }
+
+    /**
+     * @dev Returns chain id.
+     */
+    function _chainID() public view returns (uint32) {
+        uint32 chainID;
+        assembly {
+            chainID := chainid()
+        }
+        return chainID;
+    }
+
+    /**
+     * @dev Returns number of stages.
+     */
+    function getNumberStages() external view virtual returns (uint256) {
+        return mintStages.length;
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        virtual
+        override(ONFT721Core, ERC721A)
+        returns (bool)
+    {
+        return
+            interfaceId == type(IONFT721Core).interfaceId ||
+            super.supportsInterface(interfaceId);
+    }
+
+    function _debitFrom(
+        address _from,
+        uint16,
+        bytes memory,
+        uint256 _tokenId
+    ) internal virtual override(ONFT721Core) {
+        require((checkStakeEnd(_tokenId)), "TokenId_IN_STAKE");
+        safeTransferFrom(_from, address(this), _tokenId);
+    }
+
+    function _creditTo(
+        uint16,
+        address _toAddress,
+        uint256 _tokenId
+    ) internal virtual override(ONFT721Core) {
+        require(
+            !_exists(_tokenId) ||
+                (_exists(_tokenId) && ownerOf(_tokenId) == address(this))
+        );
+        if (!_exists(_tokenId)) {
+            _safeMint(_toAddress, _tokenId);
+        } else {
+            safeTransferFrom(address(this), _toAddress, _tokenId);
+        }
+    }
+
+    function onERC721Received(
+        address,
+        address,
+        uint256,
+        bytes memory
+    ) public virtual override returns (bytes4) {
+        return ERC721A__IERC721Receiver.onERC721Received.selector;
+    }
+}
